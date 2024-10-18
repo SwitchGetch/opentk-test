02f7cffec78639b3b7e490714fa4ed19cec2c2df
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2024-10-19 00:10:39 +0300

    Add files via upload

--- Game.cs
+++ Game.cs
@@ -10,7 +10,7 @@ public class Game : GameWindow
 {
 	public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
 
-    float[] vertices =
+    /*float[] vertices =
 	{
     //Position          Texture coordinates
      0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
@@ -22,9 +22,70 @@ public class Game : GameWindow
     uint[] indices = {
     0, 1, 3,
     1, 2, 3
+	};*/
+
+    float[] vertices =
+	{
+		// передняя грань
+		-0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,   0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
+
+		// задняя грань
+        -0.5f, -0.5f, -0.5f,   1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+
+		// левая грань
+        -0.5f, -0.5f,  0.5f,   1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,   1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+
+		// правая грань
+         0.5f, -0.5f,  0.5f,   0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,   0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,   1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+
+		// нижняя грань
+        -0.5f, -0.5f,  0.5f,   0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,   1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,   1.0f, 0.0f,
+
+		// верхняя грань
+        -0.5f,  0.5f,  0.5f,   0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,   1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,   1.0f, 1.0f,
+    };
+
+	uint[] indices =
+	{
+		0, 1, 2,
+		1, 2, 3,
+
+		4, 5, 6,
+		5, 6, 7,
+
+		8, 9, 10,
+		9, 10, 11,
+
+		12, 13, 14,
+		13, 14, 15,
+
+		16, 17, 18,
+		17, 18, 19,
+
+		20, 21, 22,
+		21, 22, 23
 	};
 
-	int VertexBufferObject;
+
+    int VertexBufferObject;
 	int VertexArrayObject;
 	int ElementBufferObject;
 
@@ -45,7 +106,7 @@ public class Game : GameWindow
 		shader = new Shader("shader.vert", "shader.frag");
 		shader.Use();
 
-		transform = Matrix4.Identity;
+		transform = Matrix4.CreateRotationX(-0.25f);
 		shader.SetMartix4("transform", ref transform);
 
 		texture0 = new Texture("container.jpg");
@@ -57,26 +118,31 @@ public class Game : GameWindow
         shader.SetInt("texture0", 0);
         shader.SetInt("texture1", 1);
 
+
+        VertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(VertexArrayObject);
+
+
         VertexBufferObject = GL.GenBuffer();
 		GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
 		GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
-		VertexArrayObject = GL.GenVertexArray();
-		GL.BindVertexArray(VertexArrayObject);
+        int positionLocation = shader.GetAttribLocation("aPosition");
+        GL.EnableVertexAttribArray(positionLocation);
+        GL.VertexAttribPointer(positionLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
 
-		int positionLocation = shader.GetAttribLocation("aPosition");
-		GL.VertexAttribPointer(positionLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
-		GL.EnableVertexAttribArray(positionLocation);
-
-		int texCoordLocation = shader.GetAttribLocation("aTexCoord");
-		GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
-		GL.EnableVertexAttribArray(texCoordLocation);
+        int texCoordLocation = shader.GetAttribLocation("aTexCoord");
+        GL.EnableVertexAttribArray(texCoordLocation);
+        GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
         ElementBufferObject = GL.GenBuffer();
-		GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-		GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
 
         shader.SetVector2("u_resolution", (Vector2)ClientSize);
+
+        GL.Enable(EnableCap.DepthTest);
 
         timer.Start();
 	}
@@ -85,14 +151,12 @@ public class Game : GameWindow
 	{
 		base.OnUpdateFrame(args);
 
-		transform *= Matrix4.CreateRotationZ((float)args.Time);
+		transform *= Matrix4.CreateRotationY((float)args.Time);
 		shader.SetMartix4("transform", ref transform);
-
-        float time = (float)timer.Elapsed.TotalSeconds;
 		
-		shader.SetFloat("u_time", time);
+		shader.SetFloat("u_time", (float)timer.Elapsed.TotalSeconds);
 
-		Title = time.ToString();
+		Title = "FPS: " + ((uint)(1f / args.Time)).ToString();
 
 		if (KeyboardState.IsKeyDown(Keys.Escape))
 		{
@@ -104,9 +168,9 @@ public class Game : GameWindow
 	{
 		base.OnRenderFrame(args);
 
-		GL.Clear(ClearBufferMask.ColorBufferBit);
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-		GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+        GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
 
 		SwapBuffers();
 	}
